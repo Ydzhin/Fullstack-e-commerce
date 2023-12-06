3d5d802a06bab89ebd44e1da997fcaea5e403f6d
Ydzhin <popkin.02@list.ru>
2023-12-06 17:47:28 +0100

    Update ts working  app

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-const path = require('path')
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import * as path from "path";
+
+const pathSrc = path.resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      "@": `${pathSrc}/`,
+      "~/": `${pathSrc}/`,
     },
   },
 });
