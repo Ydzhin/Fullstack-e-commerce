3f700589215bda5b4f483643de6ba12314ff2b65
Ydzhin <popkin.02@list.ru>
2024-01-04 15:58:07 +0100

    Add user validator

---
+++ api/src/user/validators/is-user-already-exist.validator.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { Repository } from 'typeorm';
+
+import { User } from '../entities/user.entity';
+@ValidatorConstraint({ name: 'isUserAlreadyExist', async: true })
+@Injectable()
+export class isUserAlreadyExist implements ValidatorConstraintInterface {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async validate(email: string): Promise<boolean> {
+    const user = await this.userRepository.findOneBy({ email });
+    return user === null || user === undefined;
+  }
+  defaultMessage(): string {
+    return 'The email «$value» is already register.';
+  }
+}
