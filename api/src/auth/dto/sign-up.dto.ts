afbe16d0787f93017a0003b46dc51362cdfc6e48
Ydzhin <popkin.02@list.ru>
2024-01-14 16:00:37 +0100

    Add sign-up dto

---
+++ api/src/auth/dto/sign-up.dto.ts
@@ -0,0 +1,23 @@
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsEmail,
+  MinLength,
+  Validate,
+} from 'class-validator';
+import { isUserAlreadyExist } from '../../user/validators/is-user-already-exist.validator';
+export class SignUp {
+  @IsDefined()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsDefined()
+  @IsEmail()
+  @Validate(IsUserAlreadyExist)
+  readonly email: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @MinLength(8)
+  readonly password: string;
+}
