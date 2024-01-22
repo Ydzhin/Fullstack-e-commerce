937c50201f58803d90b6170f314151285d8b458e
Ydzhin <popkin.02@list.ru>
2024-01-22 20:43:02 +0100

    Add user service

---
+++ api/src/user/services/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { FindOneOptions, Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserUpdate } from '../dto/user-update.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create(data: Partial<User>): Promise<User> {
+    const user = this.userRepository.create(data);
+    return this.userRepository.save(user);
+  }
+  async findOne(where: FindOneOptions<User>): Promise<User> {
+    const user = await this.userRepository.findOne(where);
+
+    if (!user) {
+      throw new NotFoundException(
+        `There isn't any user with identifier: ${where}`,
+      );
+    }
+    return user;
+  }
+  async update(id: number, updates: UserUpdate): Promise<User> {
+    const user = await this.userRepository.findOne({ id });
+    if (!user) {
+      throw new NotFoundException(`There isn't any user with id: ${id}`);
+    }
+    this.userRepository.merge(user, updates);
+    return this.userRepository.save(user);
+  }
+}
