43b8761d1be35e3872894db67c2c9598334ac6e0
Yauheni <evgeniyp180@gmail.com>
2024-01-31 20:28:45 +0100

    add jwt strategy

---
+++ api/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, JwtFromRequestFunction, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { User } from 'src/user/entities/user.entity';
+
+const extractJwtFromCookie: JwtFromRequestFunction = request => {
+  return request.signedCookies['token']!;
+};
+export class JWTStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        extractJwtFromCookie,
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]),
+      secretOrKey: process.env.APP_SECRET,
+      ignoreExpiration: false,
+      passReqToCallback: false,
+    });
+  }
+  validate(payload: JwtPayload): Promise<User> {
+    return this.authService.verifyPayload(payload);
+  }
+}
