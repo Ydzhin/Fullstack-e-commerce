e484a67ae94dfd00dbfb9b49499796417ad8cbba
Yauheni <evgeniyp180@gmail.com>
2024-01-31 20:35:19 +0100

    add injectability

--- api/src/auth/strategies/jwt.strategy.ts
+++ api/src/auth/strategies/jwt.strategy.ts
@@ -3,10 +3,12 @@ import { ExtractJwt, JwtFromRequestFunction, Strategy } from 'passport-jwt';
 import { AuthService } from '../auth.service';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
 import { User } from 'src/user/entities/user.entity';
+import { Injectable } from '@nestjs/common';
 
 const extractJwtFromCookie: JwtFromRequestFunction = request => {
   return request.signedCookies['token']!;
 };
+@Injectable()
 export class JWTStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private readonly authService: AuthService) {
     super({
