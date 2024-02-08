61784ca47dc615a58b15a5eb68617edee552e129
Yauheni <evgeniyp180@gmail.com>
2024-02-08 18:56:45 +0100

    add user factory

---
+++ api/src/user/factories/user.factory.ts
@@ -0,0 +1,9 @@
+import { setSeederFactory } from 'typeorm-extension';
+import { User } from '../entities/user.entity';
+export const userFactory = setSeederFactory(User, faker => {
+  const user = new User();
+  user.name = faker.person.fullName();
+  user.email = faker.internet.email();
+  user.password = 'Pa$$w0rd';
+  return user;
+});
