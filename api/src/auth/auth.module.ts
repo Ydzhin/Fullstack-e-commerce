2ff3e7527d31bcd6dd2ec59f09d7f3046b4fd000
Yauheni <evgeniyp180@gmail.com>
2024-02-29 17:25:43 +0100

    add all for auth module

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { SessionSerializer } from './session.serializer';
+import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.APP_SECRET,
+      signOptions: {
+        expiresIn: '1d',
+        algorithm: 'HS384',
+      },
+      verifyOptions: {
+        algorithms: ['HS384'],
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, SessionSerializer],
+  providers: [AuthService, LocalStrategy, JwtStrategy, SessionSerializer],
 })
 export class AuthModule {}
