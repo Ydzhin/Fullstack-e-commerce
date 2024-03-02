73bfbb7e1b21a0230a76927bfe21662472eaead8
Yauheni <evgeniyp180@gmail.com>
2024-03-02 17:12:51 +0100

    add todo filter

---
+++ api/src/todo/filters/todo.filters.ts
@@ -0,0 +1,28 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { EntityNotFoundError } from 'typeorm';
+import type { Request, Response } from 'express';
+
+class TodoNotFoundConstraintErrorToCatch {
+  static [Symbol.hasInstance](instance: unknown) {
+    return instance instanceof EntityNotFoundError;
+  }
+}
+
+@Catch(TodoNotFoundConstraintErrorToCatch)
+export class TodoFilter implements ExceptionFilter<EntityNotFoundError> {
+  catch(_: EntityNotFoundError, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse<Response>();
+    const request = context.getRequest<Request>();
+    response.status(HttpStatus.NOT_FOUND).json({
+      statusCode: HttpStatus.NOT_FOUND,
+      message: `Not found any todo with id: ${request.params.id}`,
+      error: 'Todo Not Found',
+    });
+  }
+}
