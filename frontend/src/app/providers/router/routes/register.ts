e07b0f2d5a157662acbe728f8e7ab2d9f86d4b65
Yauheni <evgeniyp180@gmail.com>
2024-09-10 20:12:58 +0300

    add register

--- frontend/src/app/providers/router/routes/register.ts
+++ frontend/src/app/providers/router/routes/register.ts
@@ -1,3 +1,5 @@
+import { LayoutEnum } from "../../AppRoutes";
+
 import type { RouteRecordRaw } from "vue-router";
 
 export const routeName: string = "RegisterPage";
@@ -5,5 +7,8 @@ export const routeName: string = "RegisterPage";
 export const route: RouteRecordRaw = {
   name: routeName,
   path: "/register",
+  meta: {
+    layout: LayoutEnum.common,
+  },
   component: () => import("@/pages/register"),
 };
