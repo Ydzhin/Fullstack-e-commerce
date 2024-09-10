dd09ce982bc2480b914ee75765cd528e7808122a
Yauheni <evgeniyp180@gmail.com>
2024-09-10 20:09:42 +0300

    add layouts

---
+++ frontend/src/shared/ui/layouts/common.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="layout__default">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+// console.log("vvv");
+</script>
---
+++ frontend/src/shared/ui/layouts/index.ts
@@ -0,0 +1,2 @@
+export { default as secured } from "@/shared/ui/layouts/secured.vue";
+export { default as common } from "@/shared/ui/layouts/common.vue";
