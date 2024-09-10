57c46b403731fe501c28900360df6d7302ca9d0c
Yauheni <evgeniyp180@gmail.com>
2024-09-10 20:12:06 +0300

    add login

--- frontend/src/app/providers/router/routes/login.ts
+++ frontend/src/app/providers/router/routes/login.ts
@@ -1,3 +1,5 @@
+import { LayoutEnum } from "../../AppRoutes";
+
 import type { RouteRecordRaw } from "vue-router";
 
 export const routeName: string = "LoginPage";
@@ -5,5 +7,8 @@ export const routeName: string = "LoginPage";
 export const route: RouteRecordRaw = {
   name: routeName,
   path: "/login",
+  meta: {
+    layout: LayoutEnum.common,
+  },
   component: () => import("@/pages/login"),
 };
