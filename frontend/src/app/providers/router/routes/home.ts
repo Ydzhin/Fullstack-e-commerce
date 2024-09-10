92ec04553f5711e27b8b2ab8851196fc0ef51995
Yauheni <evgeniyp180@gmail.com>
2024-09-10 20:11:50 +0300

    add home route

--- frontend/src/app/providers/router/routes/home.ts
+++ frontend/src/app/providers/router/routes/home.ts
@@ -1,3 +1,5 @@
+import { LayoutEnum } from "../../AppRoutes";
+
 import type { RouteRecordRaw } from "vue-router";
 
 export const routeName = "HomePage";
@@ -5,5 +7,8 @@ export const routeName = "HomePage";
 export const route: RouteRecordRaw = {
   name: routeName,
   path: "/",
+  meta: {
+    layout: LayoutEnum.common,
+  },
   component: () => import("@/pages/home"),
 };
