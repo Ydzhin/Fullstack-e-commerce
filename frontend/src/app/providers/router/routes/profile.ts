fcf9ef690e964b6e09d3bd510e20596eaa9a0916
Yauheni <evgeniyp180@gmail.com>
2024-09-10 20:12:42 +0300

    add profile

--- frontend/src/app/providers/router/routes/profile.ts
+++ frontend/src/app/providers/router/routes/profile.ts
@@ -1,3 +1,5 @@
+import { LayoutEnum } from "../../AppRoutes";
+
 import type { RouteRecordRaw } from "vue-router";
 
 export const routeName = "ProfilePage";
@@ -5,5 +7,8 @@ export const routeName = "ProfilePage";
 export const route: RouteRecordRaw = {
   name: routeName,
   path: "/profile",
+  meta: {
+    layout: LayoutEnum.common,
+  },
   component: () => import("@/pages/profile"),
 };
