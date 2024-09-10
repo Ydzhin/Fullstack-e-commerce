e6749e8b4b4794124ba5288569fd72a57b9e92e4
Yauheni <evgeniyp180@gmail.com>
2024-09-10 20:13:35 +0300

    add newspge

--- frontend/src/pages/news/ui/NewsPage.vue
+++ frontend/src/pages/news/ui/NewsPage.vue
@@ -1,11 +1,28 @@
+<template>
+  <div>
+    <h3 class="mt-3 text-center">
+      Недавние записи
+    </h3>
+    <div class="mt-5">
+      <News
+        v-for="(item, index) in postsData"
+        :key="index"
+        :_id="item._id"
+        :author_id="item.author_id"
+        :text="item.text"
+        :created-at="item.createdAt"
+      />
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
 import axios from "axios";
 import { decode } from "jwt-js-decode";
 import { onMounted, ref, type Ref } from "vue";
 
 import News from "@/components/NewsPage/News/News.vue";
-
-import { api } from "../../Constants";
+import { api } from "@/Constants";
 
 interface IPostsData {
   _id?: string;
@@ -29,23 +46,6 @@ onMounted(() => {
 });
 
 </script>
-
-<template>
-  <div>
-    <h3 class="mt-3 text-center">
-      Недавние записи
-    </h3>
-    <div class="mt-5">
-      <News
-        v-for="(item) in postsData"
-        :_id="item._id"
-        :author_id="item.author_id"
-        :text="item.text"
-        :created-at="item.createdAt"
-      />
-    </div>
-  </div>
-</template>
 
 <style scoped>
 h3 {
