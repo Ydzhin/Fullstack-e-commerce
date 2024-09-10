c3e44188bed19de6fd161470b57d8226f903848d
Yauheni <evgeniyp180@gmail.com>
2024-09-10 20:15:01 +0300

    add providers

--- frontend/src/app/providers/stores/index.ts
+++ frontend/src/app/providers/stores/index.ts
@@ -1,13 +1,57 @@
-// import { defineStore } from "pinia";
+import axios from "axios";
+import { decode } from "jwt-js-decode";
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
 
-// import { useResizeObserver } from "@/shared/lib/use/useResizeObserver";
+import { setItem } from "@/shared/lib/utils/localstorage";
 
-// const namespace = "app";
+interface userData {
+  _id?: string;
+  password?: string;
+  login?: string;
+}
 
-// export const useAppStore = defineStore(namespace, () => {
-//   const { screenWidth } = useResizeObserver(document.body);
+export const useAuthStore = defineStore("auth", () => {
+  const currentUser = ref<userData>(null);
+  const isLoggedIn = ref<boolean>(false);
 
-//   return {
-//     screenWidth,
-//   };
-// });
+  async function authUser({ login, password }: { login: string; password: string }): Promise<void> {
+    try {
+      const resp = await axios.post(`${import.meta.env.VITE_APP_API}/auth/sign_in`, {
+        login,
+        password,
+      });
+      const { token } = resp.data;
+      setItem("token", token);
+
+      if (token) {
+        isLoggedIn.value = true;
+      }
+    } catch (err) {
+      console.log("lalka");
+      error();
+    }
+  }
+
+  const hasToken = computed(() => {
+    const token: string = localStorage.getItem("token") ?? "";
+
+    return !!token;
+  });
+
+  async function getUserByToken() {
+    const token: string = localStorage.getItem("token")!;
+    const jwt = decode(token);
+    const { id } = jwt.payload;
+    axios.get(`http://localhost:3001/user/get_by_id/${id}`).then((resp) => {
+      currentUser.value = resp.data;
+      isLoggedIn.value = true;
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  return {
+    currentUser, isLoggedIn, authUser, hasToken, getUserByToken,
+  };
+});
