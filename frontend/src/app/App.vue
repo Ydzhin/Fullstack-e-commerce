cf5629f69019651aff2560ccb7302b016c733e33
Yauheni <evgeniyp180@gmail.com>
2024-09-12 16:26:19 +0300

    add tailwind

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -1,41 +1,24 @@
 <template>
   <div class="wrapperApp">
-    <div class="content">
-      <!-- <HeaderApp v-if="this.$route.name !== 'AuthPage' && this.$route.name !== 'RegisterPage'" /> -->
-      <div class="container mt-4">
-        <Suspense>
-          <auth-layer />
-        </Suspense>
-      </div>
-    </div>
+    <Suspense>
+      <auth-layer />
+    </Suspense>
   </div>
 </template>
 <script setup lang="ts">
 import { AuthLayer } from "@/shared/ui/auth";
-// import HeaderApp from "./components/Header/HeaderApp.vue";
 </script>
 
 <style>
-.alert {
-  width: 400px;
+html{
+  height: 100%;
 }
-
-button {
-  background: none;
-  border: none;
+body{
+  height: 100%;
 }
-
-input,
-textarea {
-  border: none;
-  background: none;
+#app{
+  height: 100%;
 }
-
-body {
-  background-color: #f9f9f9;
-  overflow: hidden;
-}
-
 .wrapperApp {
   background-image: url("https://i.pinimg.com/564x/3c/a9/7e/3ca97e8538585ba87eed3b0ce36960f6.jpg");
   width: 100%;
@@ -43,30 +26,8 @@ body {
   background-position: center;
   background-size: cover;
   background-repeat: repeat;
-  height: 973px;
+  height: 100%;
+  /* height: 973px; */
 }
 
-.content {
-  position: absolute;
-  top: 0;
-  height: 950px;
-  z-index: 2;
-  width: 100%;
-  overflow-x: hidden;
-  overflow: scroll;
-}
-
-.content::-webkit-scrollbar {
-  display: none;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity .50s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
 </style>
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,7 @@
+export default {
+  content: ["./src/**/*.{html,js,ts,vue}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
